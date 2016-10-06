71a495cdbda0e1ff83d6832b1fad9b0543d11e17
Geet Gobind Singh <geetgobindsingh@gmail.com>
2016-10-07 02:52:56 +0530

    Adding Firbase Library

---
+++ app/src/main/java/com/learncircle/geet/CuratedListApplication.java
@@ -0,0 +1,8 @@
+package com.learncircle.geet;
+
+/**
+ * Created by Geet on 07-10-2016.
+ */
+
+public class CuratedListApplication {
+}
---
+++ app/src/main/java/com/learncircle/geet/util/Globals.java
@@ -0,0 +1,8 @@
+package com.learncircle.geet.util;
+
+/**
+ * Created by Geet on 07-10-2016.
+ */
+
+public class Globals {
+}
