3af63e10634d87831453684dc5e3e364d2849a6e
Geet Gobind Singh <geetgobindsingh@gmail.com>
2016-10-31 02:14:26 +0530

    Necessary Updates

--- app/src/main/java/com/learncircle/geet/widget/CuratedTextView.java
+++ app/src/main/java/com/learncircle/geet/widget/CuratedTextView.java
@@ -8,16 +8,16 @@ import android.widget.TextView;
  * Created by Geet on 31-10-2016.
  */
 
-public class CurratedTextView extends TextView {
-    public CurratedTextView(Context context) {
+public class CuratedTextView extends TextView {
+    public CuratedTextView(Context context) {
         super(context);
     }
 
-    public CurratedTextView(Context context, AttributeSet attrs) {
+    public CuratedTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public CurratedTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public CuratedTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
