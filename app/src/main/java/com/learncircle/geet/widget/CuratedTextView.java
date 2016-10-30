3b1a1feedb1f2321c5ae9a15e24ebf8123622ee6
Geet Gobind Singh <geetgobindsingh@gmail.com>
2016-10-31 02:14:01 +0530

    Necessary Updates

---
+++ app/src/main/java/com/learncircle/geet/widget/CuratedTextView.java
@@ -0,0 +1,24 @@
+package com.learncircle.geet.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Geet on 31-10-2016.
+ */
+
+public class CurratedTextView extends TextView {
+    public CurratedTextView(Context context) {
+        super(context);
+    }
+
+    public CurratedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CurratedTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
